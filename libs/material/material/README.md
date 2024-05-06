e9d68d4d026ff997da92d171e0881608c41a2f86
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-05-06 16:42:14 -0500

    material packages

---
+++ libs/material/material/README.md
@@ -0,0 +1,7 @@
+# material-material
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test material-material` to execute the unit tests.
---
+++ libs/material/material/src/lib/material-material/material-material.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-material-material',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './material-material.component.html',
+  styleUrl: './material-material.component.css',
+})
+export class MaterialMaterialComponent {}
