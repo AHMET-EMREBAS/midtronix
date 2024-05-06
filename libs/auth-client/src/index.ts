8a2aac8208973b5a76cbf0b3e3a491f118a88c10
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-05-06 18:38:28 -0500

    auth-client package is generated

---
+++ libs/auth-client/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/auth-client/auth-client.component';
---
+++ libs/auth-client/src/lib/auth-client/auth-client.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-auth-client',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './auth-client.component.html',
+  styleUrl: './auth-client.component.css',
+})
+export class AuthClientComponent {}
