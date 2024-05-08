e4d3ab501b46a754bf1f7a0bb292a4e108306b0a
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-05-08 14:33:34 -0500

    input directive

--- libs/material/core/src/lib/directive/index.ts
+++ libs/material/core/src/lib/directive/index.ts
@@ -2,3 +2,4 @@ export * from './viewport.directive';
 export * from './fullscreen.directive';
 export * from './logo.directive';
 export * from './typing.directive';
+export * from './input.directive';
