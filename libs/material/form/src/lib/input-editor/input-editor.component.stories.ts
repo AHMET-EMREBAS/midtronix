020e809e15257562c7f7153fb236866a049f83ae
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-05-09 11:56:24 -0500

    quill text editor

--- libs/material/form/src/lib/input-editor/input-editor.component.stories.ts
+++ libs/material/form/src/lib/input-editor/input-editor.component.stories.ts
@@ -8,6 +8,7 @@ import { InputEditorComponent } from './input-editor.component';
 import { within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { FormControl } from '@angular/forms';
 
 const meta: Meta<InputEditorComponent> = {
   component: InputEditorComponent,
@@ -21,14 +22,20 @@ const meta: Meta<InputEditorComponent> = {
 export default meta;
 type Story = StoryObj<InputEditorComponent>;
 
+const editorControl = new FormControl('', []);
+
 export const Primary: Story = {
-  args: {},
+  args: {
+    label: 'Post',
+    formControl: editorControl,
+    inputName: 'editor',
+  },
 };
 
 export const Heading: Story = {
-  args: {},
+  args: Primary.args,
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-    expect(canvas.getByText(/input-editor works!/gi)).toBeTruthy();
+    expect(canvas.getByText(/Post/gi)).toBeTruthy();
   },
 };
--- libs/material/form/src/lib/input-editor/input-editor.component.ts
+++ libs/material/form/src/lib/input-editor/input-editor.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { InputBaseComponent } from '../input-base';
+import Quill from 'quill';
 @Component({
   selector: 'mdtx-input-editor',
   standalone: true,
@@ -8,4 +9,27 @@ import { CommonModule } from '@angular/common';
   templateUrl: './input-editor.component.html',
   styleUrl: './input-editor.component.scss',
 })
-export class InputEditorComponent {}
+export class InputEditorComponent
+  extends InputBaseComponent
+  implements AfterViewInit
+{
+  @ViewChild('editor') editorContainer!: ElementRef<HTMLDivElement>;
+
+  editor: any;
+
+  override ngAfterViewInit(): void {
+    super.ngAfterViewInit();
+
+    const editor = new Quill(this.editorContainer.nativeElement, {
+      theme: 'snow',
+    });
+
+    editor.on('text-change', (value: string) => {
+      this.formControl.setValue(value);
+    });
+
+    this.formControl.valueChanges.subscribe((value) => {
+      console.table({ controlValue: value });
+    });
+  }
+}
