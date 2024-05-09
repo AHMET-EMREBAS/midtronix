c514dcd40db789b8895da34ab07674741bba6c89
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-05-09 16:05:48 -0500

    table row select

--- libs/material/table/src/lib/table/table.component.ts
+++ libs/material/table/src/lib/table/table.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
-
+import {
+  MatCheckboxChange,
+  MatCheckboxModule,
+} from '@angular/material/checkbox';
 export interface PeriodicElement {
+  id: number;
   name: string;
   position: number;
   weight: number;
@@ -11,25 +15,70 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { id: 1, position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { id: 2, position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { id: 3, position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { id: 4, position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { id: 5, position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { id: 6, position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { id: 7, position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { id: 8, position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { id: 9, position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { id: 10, position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
 ];
+
 @Component({
   selector: 'mdtx-table',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatSortModule],
+  imports: [CommonModule, MatCheckboxModule, MatTableModule, MatSortModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
 })
 export class TableComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  readonly firstColumns = ['first'];
+  readonly lastColumns = ['last'];
+
+  readonly selectedItems = new Map();
+
+  @Input() columns: string[] = ['id', 'position', 'name', 'weight', 'symbol'];
+  @Input() displayedColumns: string[] = [
+    ...this.firstColumns,
+    'position',
+    'name',
+    'weight',
+    'symbol',
+    ...this.lastColumns,
+  ];
+  @Input() dataSource = ELEMENT_DATA;
+
+  selectItem(event: MatCheckboxChange, item: any) {
+    if (event.checked) {
+      this.selectedItems.set(item.id, item);
+    } else {
+      this.selectedItems.delete(item.id);
+    }
+    console.log([...this.selectedItems.entries()]);
+  }
+
+  selectAll(event: MatCheckboxChange) {
+    if (event.checked) {
+      this.dataSource.forEach((e) => {
+        this.selectItem(event, e);
+      });
+    } else {
+      this.dataSource.forEach((e) => {
+        this.selectItem(event, e);
+      });
+    }
+  }
+
+  isAllSelected() {
+    return this.dataSource.length === this.selectedItems.size;
+  }
+  isPartialSelected() {
+    return (
+      this.selectedItems.size > 0 &&
+      this.selectedItems.size < this.dataSource.length
+    );
+  }
 }
