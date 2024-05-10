6a913675bce0d605403efd02d35b81948d6ebbde
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-05-10 17:13:16 -0500

    module configurations

--- libs/modules/src/index.ts
+++ libs/modules/src/index.ts
@@ -1,11 +1,1 @@
-export * from '@mdtx/modules/auth';
-export * from '@mdtx/modules/cms';
-export * from '@mdtx/modules/ems';
-export * from '@mdtx/modules/inventory';
-export * from '@mdtx/modules/landing-page';
-export * from '@mdtx/modules/pms';
-export * from '@mdtx/modules/pos';
-export * from '@mdtx/modules/website';
-
-
-
+export const ____modules_module___ = '';
