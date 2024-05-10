4dcd4d99901b952473c4e85f8db2b787dcd59f40
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-05-10 10:59:06 -0500

    verticle and horizontal scrolling

--- libs/material/core/src/lib/directives/fullscreen.component.stories.ts
+++ libs/material/core/src/lib/directives/fullscreen.component.stories.ts
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 import { FullscreenDirectiveComponent } from './fullscreen.component';
 import { within } from '@storybook/testing-library';
-import { expect } from '@storybook/jest';
 
 const meta: Meta<FullscreenDirectiveComponent> = {
   component: FullscreenDirectiveComponent,
--- libs/material/core/src/lib/directives/scroll.directive.ts
+++ libs/material/core/src/lib/directives/scroll.directive.ts
@@ -1,29 +1,46 @@
-import { Directive, HostListener } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  Input,
+  OnInit,
+} from '@angular/core';
 
 @Directive({
   selector: '[mdtxScroll]',
   standalone: true,
 })
-export class ScrollDirective {
+export class ScrollDirective implements OnInit {
+  @Input() mdtxScroll?: 'x' | 'y';
+
   startX: number | null = null;
   startY: number | null = null;
 
-  @HostListener('mousedown')
+  @HostListener('mousedown', ['$event'])
   mousedownHandler(event: any) {
+    console.log('Mouse Down: ', event);
     this.startX = event.pageX;
     this.startY = event.pageY;
   }
 
-  @HostListener('mousemove')
+  @HostListener('mousemove', ['$event'])
   mousemoveHandler(event: any) {
+    console.log('Mouse Move: ', event);
     if (this.startX !== null && this.startY !== null) {
       const deltaX = this.startX - event.pageX;
       const deltaY = this.startY - event.pageY;
 
       // If horizontal scroll detected, prevent default vertical scroll behavior
-      if (Math.abs(deltaX) > Math.abs(deltaY)) {
-        event.preventDefault();
-        window.scrollBy(deltaX, 0);
+      if (this.mdtxScroll === 'x') {
+        if (Math.abs(deltaX) > Math.abs(deltaY)) {
+          event.preventDefault();
+          this.ref.nativeElement.scrollBy(deltaX, 0);
+        }
+      } else if (this.mdtxScroll === 'y') {
+        // if (Math.abs(deltaY) < Math.abs(deltaX)) {
+          event.preventDefault();
+          this.ref.nativeElement.scrollBy(0, deltaY);
+        // }
       }
 
       this.startX = event.pageX;
@@ -35,5 +52,16 @@ export class ScrollDirective {
   mouseupHander() {
     this.startX = null;
     this.startY = null;
+  }
+
+  @HostListener('scroll', ['$event'])
+  scrollHander(event: any) {
+    console.log('Scroll: ', event);
+  }
+
+  constructor(protected readonly ref: ElementRef<HTMLElement>) {}
+
+  ngOnInit(): void {
+    if (!this.mdtxScroll) this.mdtxScroll = 'y';
   }
 }
