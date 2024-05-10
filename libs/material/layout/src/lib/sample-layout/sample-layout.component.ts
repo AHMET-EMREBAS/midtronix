663ca7e4c4b666d715a73f38f047e422ee9ef0b6
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-05-10 14:27:42 -0500

    layout component is ready to use

--- libs/material/layout/src/lib/sample-layout/sample-layout.component.ts
+++ libs/material/layout/src/lib/sample-layout/sample-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from '../layout/layout.module';
-
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'mdtx-sample-layout',
   standalone: true,
-  imports: [CommonModule, LayoutModule],
+  imports: [CommonModule, LayoutModule, MatListModule, MatIconModule],
   templateUrl: './sample-layout.component.html',
   styleUrl: './sample-layout.component.scss',
 })
