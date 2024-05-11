f14f4fce1a1cf84f58017828f05d0f06fb3c85c8
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-05-11 09:09:02 -0500

    cleanup

--- libs/material/layout/src/lib/layout/base-layout.component.spec.ts
+++ libs/material/layout/src/lib/layout/base-layout.component.spec.ts
@@ -1,19 +1,5 @@
 describe('BaseLayoutComponent', () => {
   it('should work', () => {
-    let result = '';
-    ['Toolbar', 'Statusbar'].forEach((e) => {
-      ['Left', 'Right', 'Center'].forEach((s) => {
-        result += `@Input() ${e}${s}?:Navlist; ` + '\n';
-      });
-    });
-    // @Input() toolbarLeftItems?: Navlist;
-    ['Content', 'SidenavRight', 'SidenavLeft'].forEach((e) => {
-      ['Top', 'Bottom', 'Center'].forEach((s) => {
-        result += `@Input() ${e}${s}?:Navlist; ` + '\n';
-      });
-    });
-
-    console.log(result);
     expect(1).toBe(1);
   });
 });
