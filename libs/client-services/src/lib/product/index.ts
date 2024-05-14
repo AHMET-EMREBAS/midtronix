6b4dc61682fa14dc3a5299fdf9170fd4c2d7780c
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-05-13 19:05:47 -0500

    product module

--- libs/client-services/src/lib/product/index.ts
+++ libs/client-services/src/lib/product/index.ts
@@ -5,5 +5,6 @@ export * from './product.component';
 export * from './product.routes';
 export * from './product.service';
 export * from './report/report.component';
-export * from './view/table/table.component';
+export * from './view/card-view/card-view.component';
+export * from './view/table-view/table-view.component';
 export * from './view/view.component';
--- libs/client-services/src/lib/product/view/card-view/card-view.component.ts
+++ libs/client-services/src/lib/product/view/card-view/card-view.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { ProductForm } from '../../../managers';
+import { ProductService } from '../../product.service';
+import { CommonFormModule } from '@mdtx/material/form';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 @Component({
   selector: 'mdtx-card-view',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonFormModule,
+    MatToolbarModule,
+    MatPaginatorModule,
+    RouterModule,
+    MatCardModule,
+  ],
   templateUrl: './card-view.component.html',
   styleUrl: './card-view.component.scss',
+  providers: [ProductService],
 })
-export class CardViewComponent {}
+export class CardViewComponent implements AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  readonly columns = ProductForm.columns();
+
+  displayedColumns = ProductForm.displayedColumns();
+
+  constructor(protected readonly service: ProductService) {}
+
+  ngAfterViewInit(): void {
+    this.service.getWithQuery({
+      take: this.paginator.pageSize,
+      skip: this.paginator.pageIndex * this.paginator.pageSize,
+    });
+  }
+}
