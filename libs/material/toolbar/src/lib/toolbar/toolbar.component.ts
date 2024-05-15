e0c9557f487fe0c4828f3745c58d4d1537525f92
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-05-15 15:13:33 -0500

    toolbar component

--- libs/material/toolbar/src/lib/toolbar/toolbar.component.ts
+++ libs/material/toolbar/src/lib/toolbar/toolbar.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MenuItem } from '@mdtx/material/core';
 @Component({
   selector: 'mdtx-toolbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss',
 })
-export class ToolbarComponent {}
+export class ToolbarComponent {
+  @Input() bgColor?: string;
+  @Input() leftToolbarItems?: MenuItem[];
+  @Input() rightToolbarItems?: MenuItem[];
+
+  @Output() toolbarItemClickEvent = new EventEmitter<MenuItem>();
+}
