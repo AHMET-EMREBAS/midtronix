907a95c224fcb8ec3ac4c895056a241b563ceefa
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-05-20 09:42:30 -0500

    product small card component

--- libs/material/card/src/lib/index.ts
+++ libs/material/card/src/lib/index.ts
@@ -1,2 +1,3 @@
 // @index('./*/*.component.ts', f => `export * from '${f.path}'`)
-export * from './card-avatar/card-avatar.component';
+export * from './card-avatar/card-avatar.component'
+export * from './product-card/product-card.component'
---
+++ libs/material/card/src/lib/product-small-card/product-small-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'mdtx-product-small-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-small-card.component.html',
+  styleUrl: './product-small-card.component.scss',
+})
+export class ProductSmallCardComponent {}
