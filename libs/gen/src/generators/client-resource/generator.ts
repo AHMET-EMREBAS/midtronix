32b51dc77ccce70ba4b33396a9f9f00312518643
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-05-21 15:02:02 -0500

    client-resource generator

--- libs/gen/src/generators/client-resource/generator.ts
+++ libs/gen/src/generators/client-resource/generator.ts
@@ -1,6 +1,7 @@
 import { formatFiles, generateFiles, names, Tree } from '@nx/devkit';
 import * as path from 'path';
 import { ClientResourceGeneratorSchema } from './schema';
+import { plural } from '@mdtx/utils';
 
 export async function clientResourceGenerator(
   tree: Tree,
@@ -10,6 +11,7 @@ export async function clientResourceGenerator(
   const allNames = names(options.name);
   generateFiles(tree, path.join(__dirname, 'files'), projectRoot, {
     ...allNames,
+    pluralClassName: plural(allNames.className),
   });
   await formatFiles(tree);
 }
--- libs/modules/sub-modules/src/lib/category/create-category/create-category.component.stories.ts
+++ libs/modules/sub-modules/src/lib/category/create-category/create-category.component.stories.ts
@@ -1,12 +1,45 @@
-import type { Meta, StoryObj } from '@storybook/angular';
+import {
+  applicationConfig,
+  type Meta,
+  type StoryObj,
+} from '@storybook/angular';
 import { CreateCategoryComponent } from './create-category.component';
-
 import { within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { storyBookHttpInterceptor } from '@mdtx/forms';
+import { provideEntityData, withEffects } from '@ngrx/data';
+import { provideMatFormFieldOptions } from '@mdtx/material/core';
 
 const meta: Meta<CreateCategoryComponent> = {
   component: CreateCategoryComponent,
   title: 'CreateCategoryComponent',
+
+  decorators: [
+    applicationConfig({
+      providers: [
+        provideAnimations(),
+        provideHttpClient(withInterceptors(storyBookHttpInterceptor)),
+        provideStore(),
+        provideEffects([]),
+        provideEntityData(
+          {
+            pluralNames: {
+              Category: 'Categories',
+            },
+            entityMetadata: {
+              Category: {},
+            },
+          },
+          withEffects()
+        ),
+        provideMatFormFieldOptions({ appearance: 'outline' }),
+      ],
+    }),
+  ],
 };
 export default meta;
 type Story = StoryObj<CreateCategoryComponent>;
