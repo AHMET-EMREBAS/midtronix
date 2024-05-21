9904842b0247e54643f8a6d1b90b571c11229752
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-05-21 15:39:23 -0500

    car resource

--- libs/forms/src/lib/form-components/cart/cart-form.component.ts
+++ libs/forms/src/lib/form-components/cart/cart-form.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { CommonFormModule, InputTextComponent } from '@mdtx/material/form';
+import { CommonFormModule } from '@mdtx/material/form';
 import { BaseFormComponent } from '../../__base';
 import { CartFormBuilder } from '../../form-builders';
+import {
+  CustomerSearchComponent,
+  UserSearchComponent,
+} from '../../search-inputs';
 
 @Component({
   selector: 'mdtx-cart-form',
   standalone: true,
-  imports: [CommonFormModule, InputTextComponent],
+  imports: [CommonFormModule, UserSearchComponent, CustomerSearchComponent],
   templateUrl: './cart-form.component.html',
   styleUrl: './cart-form.component.scss',
 })
