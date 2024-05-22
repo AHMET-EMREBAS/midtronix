822b1b056e06970dd8c3bb1cd2d2ca4acb31d92d
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-05-22 11:39:25 -0500

    end date check

--- libs/database/src/lib/entities/discount.view.ts
+++ libs/database/src/lib/entities/discount.view.ts
@@ -11,8 +11,11 @@ import { Discount } from './discount';
       .addSelect('main.fixed', 'fixed')
       .addSelect('main.percent', 'percent')
       .addSelect('ds.skuId', 'skuId')
+      .addSelect('main.startDate', 'startDate')
+      .addSelect('main.endDate', 'endDate')
       .from(Discount, 'main')
-      .leftJoin('discount_skus_sku', 'ds', 'ds.discountId = main.id');
+      .leftJoin('discount_skus_sku', 'ds', 'ds.discountId = main.id')
+      .where('main.endDate > CURRENT_DATE');
   },
 })
 export class DiscountView {
@@ -20,6 +23,8 @@ export class DiscountView {
   @ViewColumn() skuId!: number;
   @ViewColumn() name!: string;
   @ViewColumn() description!: string;
+  @ViewColumn() startDate!: Date;
+  @ViewColumn() endDate!: Date;
   @ViewColumn() fixed!: number;
   @ViewColumn() percent!: number;
 }
