46758f2fe4697e97c0e309df5505af0381573b96
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-05-23 14:31:28 -0500

    order updated

--- libs/database/src/lib/entities/order.dto.ts
+++ libs/database/src/lib/entities/order.dto.ts
@@ -1,12 +1,27 @@
 import { ICreateOrderDto, IID } from '@mdtx/common';
-import { Exclude, IDObjectProperty, PartialType, Property } from '@mdtx/core';
+import {
+  Exclude,
+  IDObjectProperty,
+  NumberTransformer,
+  PartialType,
+  Property,
+} from '@mdtx/core';
 
 @Exclude()
 export class CreateOrderDto implements ICreateOrderDto {
   @Property({ type: 'number', minimum: 1 }) quantity!: number;
-  @Property({ type: 'number' }) salePrice!: number;
-  @Property({ type: 'number' }) saleSubtotal!: number;
-  @Property({ type: 'number' }) saleTotal!: number;
+  @Property({ type: 'number' })
+  @NumberTransformer()
+  salePrice!: number;
+
+  @Property({ type: 'number' })
+  @NumberTransformer()
+  saleSubtotal!: number;
+
+  @Property({ type: 'number' })
+  @NumberTransformer()
+  saleTotal!: number;
+
   @IDObjectProperty({ required: true }) sku!: IID;
   @IDObjectProperty({ required: true }) cart!: IID;
   @IDObjectProperty({ required: true }) priceLevel!: IID;
