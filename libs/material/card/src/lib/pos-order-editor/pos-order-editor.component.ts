65ee4266d67367d8616bf4d7840cb74cbb2d2a67
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-05-24 15:34:43 -0500

    pos order editor

---
+++ libs/material/card/src/lib/pos-order-editor/pos-order-editor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'mdtx-pos-order-editor',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pos-order-editor.component.html',
+  styleUrl: './pos-order-editor.component.scss',
+})
+export class PosOrderEditorComponent {}
