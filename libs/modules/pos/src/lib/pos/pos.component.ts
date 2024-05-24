e1e185047fa780ef0273163ec2a0a53c2e1eb8e0
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-05-24 10:36:36 -0500

    pos start

---
+++ libs/modules/pos/src/lib/pos/pos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'mdtx-pos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pos.component.html',
+  styleUrl: './pos.component.scss',
+})
+export class PosComponent {}
