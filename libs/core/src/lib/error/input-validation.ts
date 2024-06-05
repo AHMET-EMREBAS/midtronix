6179bd04236b4f4f1910f8bc60d74ff85893642f
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-06-05 12:25:24 -0500

    input validation error parser

--- libs/core/src/lib/error/input-validation.ts
+++ libs/core/src/lib/error/input-validation.ts
@@ -1,15 +1,20 @@
 import { UnprocessableEntityException } from '@nestjs/common';
 import { ValidationError } from 'class-validator';
 
+export function parseValiationErrors(errors: ValidationError[]) {
+  const __errors = errors.map((e) => {
+    return { [e.property]: Object.values(e.constraints || {}) };
+  });
+
+  if (__errors.length > 0) return __errors.reduce((p, c) => ({ ...p, ...c }));
+
+  return 'Not Defined';
+}
 export class InputValidationException extends UnprocessableEntityException {
   constructor(errors: ValidationError[]) {
     super({
       message: 'Input Validation Errror',
-      errors: errors
-        .map((e) => {
-          return { [e.property]: Object.values(e.constraints || {}) };
-        })
-        .flat(),
+      errors: parseValiationErrors(errors),
     });
   }
 }
