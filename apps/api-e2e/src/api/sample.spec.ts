b4424d0bde2bc3c8c937250a89383f0a8f8523ea
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-06-06 17:05:22 -0500

    e2e testing smoke

--- apps/api-e2e/src/api/sample.spec.ts
+++ apps/api-e2e/src/api/sample.spec.ts
@@ -7,17 +7,11 @@ import {
   IUpdateSampleDto,
 } from '@mdtx/models';
 import { loginAndGetBearerToken } from '../support/login';
-import axios from 'axios';
-
-const samples: ICreateSampleDto[] = [
-  { name: 'sample 1' },
-  { name: 'sample 2' },
-  { name: 'sample 3' },
-];
 
 const API_PREFIX = `api/${ApiVersion.v1}`;
 
 describe('Sample', () => {
+  let d1, d2, d3;
   let httpClient: HttpClient<
     ISample,
     ICreateSampleDto,
@@ -35,41 +29,33 @@ describe('Sample', () => {
       IQuerySampleDto
     >('Sample', API_PREFIX, { headers: { Authorization: bearerToken } });
 
-    await httpClient.saveOne({ name: 'Test data 1' });
-    await httpClient.saveOne({ name: 'Test data 2' });
-    await httpClient.saveOne({ name: 'Test data 3' });
+    d1 = await httpClient.saveOne({ name: 'Test data 1' });
+    d2 = await httpClient.saveOne({ name: 'Test data 2' });
+    d3 = await httpClient.saveOne({ name: 'Test data 3' });
   });
 
   it(`GET api/v1/samples`, async () => {
     const data = await httpClient.findAll();
     expect(data).toBeTruthy();
-    expect(data.length).toBeGreaterThan(0);
+    expect(data.length).toBeGreaterThan(2);
   });
 
   it(`GET api/v1/sample/1`, async () => {
-    const [first, ...rest] = await httpClient.findAll({
-      take: 1,
-      name: 'cn:Test data 1',
-    });
-    const data = await httpClient.findOneById(first.id);
-
+    const data = await httpClient.findOneById(d1.id);
     expect(data.name).toBe('Test data 1');
     expect(data.name).toBeTruthy();
   });
 
   it(`PUT api/v1/sample/1`, async () => {
-    const [first] = await httpClient.findAll({ name: 'cn:Test data', take: 1 });
-
-    const result = await httpClient.updateOne(first.id, {
+    const data = await httpClient.updateOne(d1.id, {
       name: 'Test data updated',
     });
-    expect(result).toBeTruthy();
-    expect(result.name).toBe('Test data updated');
+    expect(data).toBeTruthy();
+    expect(data.name).toBe('Test data updated');
   });
 
   afterAll(async () => {
-    const foundItems = await httpClient.findAll({ name: 'cn:Test data' });
-    for (const f of foundItems) {
+    for (const f of [d1, d2, d3]) {
       await httpClient.deleteOneById(f.id);
     }
   });
