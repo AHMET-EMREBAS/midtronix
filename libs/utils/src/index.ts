c237ef760f4cb4a2d57fb044e5c8de30fcbccb99
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-06-14 17:49:29 -0500

    building client

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,1 +1,7 @@
-export * from './lib';
+// @index(['./lib/*.ts', '!./lib/*.spec.ts'], f => `export * from '${f.path}'`)
+export * from './lib/auth-api-path-builder'
+export * from './lib/index'
+export * from './lib/names'
+export * from './lib/plural'
+export * from './lib/query'
+export * from './lib/rest-api-path-builder'
